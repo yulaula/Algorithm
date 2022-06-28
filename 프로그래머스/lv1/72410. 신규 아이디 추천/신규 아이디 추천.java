406d3da17efb8c9b0966e3debac3b997b5aae9a6
yulaula <93673554+yulaula@users.noreply.github.com>
2022-06-28 09:03:37 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.23 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -8,22 +8,15 @@ class Solution {
         tmp_id = new_id.toLowerCase();
         
         //2 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거합니다.
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < tmp_id.length(); i++) {
-            char c = tmp_id.charAt(i);
-            if((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '-' || c == '_' || c == '.') {
-                sb.append(c);
-            }
-        }
-        String tmp_id2 = sb.toString();
+        tmp_id = tmp_id.replaceAll("[^-_.a-z0-9]","");
 
         
         //3 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다
         int count = 0;
         StringBuilder sb2 = new StringBuilder();
-        for(int i = 0 ; i < tmp_id2.length(); i++) {
+        for(int i = 0 ; i < tmp_id.length(); i++) {
             boolean dot = false;
-            char c = tmp_id2.charAt(i);   
+            char c = tmp_id.charAt(i);   
             if(c == '.') {
                 count++;
             } else {
@@ -37,49 +30,43 @@ class Solution {
                 sb2.append(c);
             }
         }
-        String tmp_id3 = sb2.toString();
+        tmp_id = sb2.toString();
         
       
         //4 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
         sb2.setLength(0);
-        for(int i = 0; i < tmp_id3.length(); i++) {
-            char c = tmp_id3.charAt(i);
-            if((i == 0 || i == tmp_id3.length()-1) && c == '.')  {
+        for(int i = 0; i < tmp_id.length(); i++) {
+            char c = tmp_id.charAt(i);
+            if((i == 0 || i == tmp_id.length()-1) && c == '.')  {
                 continue;
             }
             sb2.append(c);
         }
-        String tmp_id4 = sb2.toString();
+        tmp_id = sb2.toString();
         
         
         //5 빈 문자열이라면, new_id에 "a"를 대입합니다.
-        String tmp_id5 = new String(tmp_id4);
-        if(tmp_id5.length() == 0) {
-            tmp_id5 += "a";
+       if(tmp_id.equals("")) {
+            tmp_id += "a";
         }
         
         
         //6 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
         //만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
-        String tmp_id6 = new String();
-        int len = tmp_id5.length();
+        int len = tmp_id.length();
         if(len >= 16) {
-            tmp_id6 = tmp_id5.substring(0, 15);
-            if(tmp_id6.charAt(tmp_id6.length()-1) == '.') {
-                tmp_id6 = tmp_id6.substring(0, 14);
+            tmp_id = tmp_id.substring(0, 15);
+            if(tmp_id.charAt(tmp_id.length()-1) == '.') {
+                tmp_id = tmp_id.substring(0, 14);
             }
         }
         //7 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
         else if(len <= 2) {
-            tmp_id6 = tmp_id5;
             for(int i = len; i < 3; i++) {
-                tmp_id6 += (tmp_id6.charAt(len-1) + "");
+                tmp_id += (tmp_id.charAt(len-1) + "");
             }
-        }
-        else {
-           tmp_id6 = tmp_id5;
-        }        
+        }     
                
-        return tmp_id6;
+        return tmp_id;
     }
 }
