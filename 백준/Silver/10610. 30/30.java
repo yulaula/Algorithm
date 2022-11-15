766a81e875bb6c8572efebda4d0b9896aa4bad6b
yulaula <93673554+yulaula@users.noreply.github.com>
2022-11-15 23:33:07 +0900

    [Silver IV] Title: 30, Time: 1252 ms, Memory: 32004 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -1,34 +1,30 @@
 import java.util.*;
+import java.io.*;
+
 public class Main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        
-        String str = sc.nextLine();
-        String answer = check(str);
-        
-        System.out.println(answer);
-    }
-    public static String check(String str) {
-        char[] arr = new char[str.length()];
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nArr = br.readLine().split("");
+        boolean isZero = false;
         int sum = 0;
-        for(int i = 0; i < str.length(); i++) {
-            arr[i] = str.charAt(i);
-            sum += arr[i] - '0';
+        Arrays.sort(nArr);
+
+        for(String s : nArr) {
+            sum += Integer.parseInt(s);
+            if(s.equals("0")) {
+                isZero = true;
+            }
         }
-        
-        Arrays.sort(arr);
-        if(arr[0] != '0') {
-            return "-1";
+
+        if(isZero == true && sum%3 == 0) {
+            for(int i = nArr.length-1 ; i >= 0; i--) {
+                System.out.print(nArr[i]);
+            }
+            System.out.println();
+        } else {
+            System.out.println(-1);
         }
-        if(sum%3 != 0) {
-            return "-1";
-        }
-        
-        String a = new String();
-        for(int i = arr.length-1; i >= 0; i--) {
-            a += arr[i] + "";
-        }
-        
-        return a;
     }
+    
 }
